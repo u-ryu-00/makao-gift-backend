9f31635c75d7d0b726cacb778dc41e1edf2870cc
u-ryu-00 <lwh76104@gmail.com>
2023-06-10 20:56:16 +0900

    백도어 수정

--- src/main/java/kr/megaptera/makaogift/backdoor/BackdoorController.java
+++ src/main/java/kr/megaptera/makaogift/backdoor/BackdoorController.java
@@ -20,6 +20,7 @@ public class BackdoorController {
     public String setupDatabase() {
         jdbcTemplate.execute("DELETE FROM product");
         jdbcTemplate.execute("DELETE FROM account");
+        jdbcTemplate.execute("DELETE FROM order_History");
 
         jdbcTemplate.execute("" +
                 "INSERT INTO account(id, user_Id, amount)" +
@@ -41,12 +42,28 @@ public class BackdoorController {
                 " VALUES(2, '탬버린즈', '물에 닿는 순간 풍성한 거품으로 변해 피부를 부드럽게 씻어내어 자연의 향으로 감싸줍니다', 1000, '[선물포장] 바디워시 (4종 택1)', 'https://img1.kakaocdn.net/thumb/C320x320@2x.fwebp.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fproduct%2F20221124180904_d7730ce8710a45a084a62ee6c1f56766.jpg')"
         );
 
+        jdbcTemplate.execute("" +
+                "INSERT INTO product(product_Id, company, description, price, title, image_Url)" +
+                " VALUES(100, '헤라', '정교하게 피부 빈틈을 채우는 레이어리스 매트 커버가 내 피부처럼 가볍게 핏팅되어 벨벳티 스킨을 선사하는 세미 매트 쿠션입니다', 56000, '[단독] 헤라 블랙쿠션 27호(+악세서리함 추가 증정)', 'https://img1.kakaocdn.net/thumb/C320x320@2x.fwebp.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fproduct%2F20230525133108_4d30acbb75504bc6830d2eda5795e99b.jpg')"
+        );
+
+        jdbcTemplate.execute("" +
+                "INSERT INTO order_History(order_Id, address, message, product_Id, quantity, receiver, total_Price, user_Id, title, company, description, image_Url, created_At, updated_At)" +
+                " VALUES(1, '성동구', '감사합니다', 1, 1, '유정', 2000, 'a111', '[단독각인] 캔디 글레이즈 컬러밤', '입생로랑', '투명하게 녹아 맑게 빛나는 컬러 글로우 밤', 'https://img1.kakaocdn.net/thumb/C320x320@2x.fwebp.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fproduct%2F20230516143408_834e86474204499b9fb85a2a3911ddfa.jpg', '2023-06-10 13:40:40.044709','2023-06-10 13:40:40.044709')"
+        );
         return "OK";
     }
 
     @GetMapping("delete-product")
     public String deleteProduct() {
         jdbcTemplate.execute("DELETE FROM product");
+
+        return "OK";
+    }
+
+    @GetMapping("delete-order")
+    public String deleteOrder() {
+        jdbcTemplate.execute("DELETE FROM order_History");
 
         return "OK";
     }
